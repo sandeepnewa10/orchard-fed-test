041bead4a77ba73316b88134230003ebedfeea98
sandeepnewa10 <sandeepnewa@gmail.com>
2022-08-24 11:17:12 +1000

    Final test done

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ import './sass/style.scss'
 
 // Button OnClick Event
 const btns = Array.from( document.querySelectorAll('a'))
-
 btns.map(btn => btn.addEventListener("click", e => console.log(e.target)))
